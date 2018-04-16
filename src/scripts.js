6a26334aaef97dcf494e9e1c6576001b35df618f
DEBASISH SAHOO <debasish.sahoo1998@gmail.com>
2018-04-17 01:21:01 +0530

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -13,13 +13,7 @@ $(document).ready(function(){
       async:false,
       dataType:"json",
       success: function(data){
-      //console.log(data);
-   // console.log(data[1][0]);
-     //   console.log(data[2][0]);
-       // console.log(data[3][0]);
-       
-        
-      for(var i=0;i<data[1].length;i++)
+       for(var i=0;i<data[1].length;i++)
       {
         $("#output").prepend("<li><a href=" + data[3][i] + ">" + data[1][i] + "</a><p>"+ data[2][i] + "</p></li>" );
       }
@@ -32,7 +26,7 @@ $(document).ready(function(){
     });
     $("#search").keypress(function(e){
       if(e.which===13){
-        $("#searchterm").click();
+        $("#searchitem").click();
       }
     });
  
